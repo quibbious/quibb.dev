401e8b192964caf95fbf7cfe086f4db518af740a
quibb <108188557+quibbious@users.noreply.github.com>
2026-04-11 05:55:18 -0500

    Update main.js

--- quibb.dev/main.js
+++ quibb.dev/main.js
@@ -12,14 +12,9 @@ const loader = new GLTFLoader()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 const composer = new EffectComposer(renderer);
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enabled = false;
+//const controls = new OrbitControls(camera, renderer.domElement);
 const resolution = new THREE.Vector2(window.innerWidth, window.innerHeight);
-controls.target.set(0, 0, 0); 
-controls.autoRotate = true;
 
-const afterimagePass = new AfterimagePass(0.9);
-composer.addPass(afterimagePass);
 
 
 //const renderPixelatedPass = new RenderPixelatedPass(0.5, scene, camera);
@@ -38,14 +33,12 @@ directionalLight.position.z = -20
 //const LightHelper = new THREE.DirectionalLightHelper(directionalLight)
 //scene.add(LightHelper)
 
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshPhongMaterial({color: 0xDDDDDD})
+const cube = new THREE.Mesh(geometry, material)
+
 scene.add(directionalLight)
-
-
-loader.load("/assets/source/bmw_m4_modified_widebody_knitro_builds.glb", function (glb) {
-
-    scene.add(glb.scene)
-    
-})
+scene.add(cube)
 
 
 
@@ -54,14 +47,8 @@ camera.position.set(-0.5, 8, 25);
 
 
 function animate() {
-    composer.render();
-
-    controls.update(0.0007)
+    composer.render()
     
-    console.log(directionalLight.position.y)
-
-    
-
-
+    cube.rotation.x = Math.sin(45 * 1000 / 60)
 }
 renderer.setAnimationLoop(animate);
