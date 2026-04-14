e76454dc1ecf80a337828a8123fbbaf7060df279
quibb <108188557+quibbious@users.noreply.github.com>
2026-04-14 12:39:12 -0500

    Update main.js

--- quibb.dev/main.js
+++ quibb.dev/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
 
 const scene = new THREE.Scene();
 
